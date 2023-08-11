e38eecea1a87622f2b1a3ea945e984f3da6e259e
SizomanHuxley <93354693+SizomanHuxley@users.noreply.github.com>
2023-08-11 18:02:40 -0400

    add row

--- src/component/project.js
+++ src/component/project.js
@@ -4,14 +4,15 @@ import { FaGithub } from 'react-icons/fa';
 const Project = () => {
   return (
     <div className="project"> 
-      <div className="row" style={{backgroundColor: "white"}}>
+    <div className="cool" style={{backgroundColor: "white"}}>
+      <div className="row">
         <div className="col-sm-4 box-container1">
           <div
             style={{
               display: 'flex',
               height: '800vh',
               padding: '3%',
-              maxHeight: '80vh', // Set maximum height to make sure background covers entire viewport
+              maxHeight: '30vh', // Set maximum height to make sure background covers entire viewport
               minHeight: '90vh', // Set minimum height to make sure background covers entire viewport
             }}
           >
@@ -21,9 +22,9 @@ const Project = () => {
                 width: '100%',
                 maxWidth: '400px',
                 height: '100%',
-                background: 'black',
+                border: '1px solid black',
                 borderRadius: '15px',
-                color: 'white',
+                color: 'black',
                 overflow: 'auto', // Add this rule to control overflow
               }}
             >
@@ -71,29 +72,26 @@ const Project = () => {
                 overflow: 'auto', // Add this rule to control overflow
               }}
             >
-              <div style={{backgroundColor: "#FF5733", color: "black", fontSize:"20px", padding: "5px"}} id='movie-app' className="text-center"> <h1>Tail Together</h1> </div>
-              <img src={require('./pictures/tail_together.jpeg')} alt="movie-poster" className="tail-image" style={{padding: "10px 20px 0px 20px"}} />
-
-              <p style={{ padding: "10px 20px 0px 20px" }}>
-  With this app, users can effortlessly connect and chat about upcoming venues,
-  share interests, host pregame parties, or simply engage in event-related conversations.
-  Developed using <span className='skills'>Xcode</span> and <span className='skills'>Swift</span>,
-  and powered by <span className='skills'>Back4App</span> for seamless data management.
-</p>
-
+              {/* Left Column */}
+              <div style={{ backgroundColor: "#FF5733", color: "black", fontSize: "20px", padding: "5px" }} className="text-center"> <h1>Tail Together</h1> </div>
+                  <img src={require('./pictures/tail_together.jpeg')} alt="movie-poster" className="tail-image" style={{ padding: "10px 20px 0px 20px" }} />
+                  <p style={{ padding: "10px 20px 0px 20px" }}>
+                    With this IOS app, users can effortlessly connect and chat about upcoming venues,
+                    share interests, host pregame parties, or simply engage in event-related conversations.
+                    Developed using <span className='skills'>Xcode</span> and <span className='skills'>Swift</span>,
+                    and powered by <span className='skills'>Back4App</span> for seamless data management.
+                  </p>
+              {/* GitHub Button */}
               <p className="text-center">
-                {/* GitHub button */}
                 <a href="https://github.com/jsison2021/Tail-Together" target="_blank" rel="noopener noreferrer" className="github-button">
                   <FaGithub />
                   {"  "}
                   GitHub
                 </a>
               </p>
-            
             </div>
           </div>
         </div>
-
 
         <div className="col-sm-4 box-container3">
           <div
@@ -112,19 +110,20 @@ const Project = () => {
                 maxWidth: '400px',
                 height: '100%',
                 border: '1px solid black',
-                background: 'black',
                 borderRadius: '15px',
-                color: 'white',
+                color: 'black',
                 overflow: 'auto', // Add this rule to control overflow
               }}
             >
               <div style={{backgroundColor: "white", color: "black", fontSize:"30px", padding: "5px"}} id='movie-app' className="text-center"> Gilderise Enterprise </div>
               <img src={require('./pictures/gilderise.jpeg')} alt="movie-poster" className="circle-img" style={{padding: "0px 20px 0px 20px"}} />
 
-              <p style={{padding: "10px 20px 0px 20px"}}> A website that emulates a property management system where users can register for accounts, login to accounts,
-                        update personal information, store, retrieve, update, and delete Personal Inventories.
-                   <span className='skills'>Firebase and MongoDB</span>, ensuring efficient storage and retrieval of user data.
-              </p>
+              <p style={{ padding: "10px 20px 0px 20px" }}>
+                A dynamic platform where users can seamlessly register, log in,
+  and take control of their personal inventories. Developed with cutting-edge technologies including the <span className='skills'>Flux framework</span>,
+  <span className='skills'>Python</span>, and powered by the robust <span className='skills'>Firebase</span> backend,
+  this website offers a secure and intuitive space for users to store, retrieve, update, and even delete their personal inventory data.
+ </p>
 
               <p className="text-center">
                 {/* GitHub button */}
@@ -134,14 +133,29 @@ const Project = () => {
                   GitHub
                 </a>
               </p>
-            
+            </div>
             </div>
           </div>
         </div>
+        </div>
 
-
-      </div>
-    </div>
+        <div className="row text-center">
+          <div className="col-md-8 mx-auto conclusion"
+          style={{
+          fontSize: "25px",
+          color: "white",
+          padding: "15px",
+        }}
+         >
+          <p>
+      These projects showcase my dedication to creating innovative solutions using modern technologies.
+      Each application is a testament to my skills and commitment to delivering exceptional user experiences. I am excited to continue
+      exploring new possibilities and honing my abilities in software development.<br/><br/>
+      Additionally, I have other projects on my GitHub account that are part of a private classroom repository. While you won't be able to see these projects directly, I can provide screenshots to give you a glimpse of the work I've done in those private repositories.
+    </p>
+          </div>
+          </div>
+          </div>
   );
 }
 
